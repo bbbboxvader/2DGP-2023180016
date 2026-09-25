406291942d5be07f56017e0e77d703feafbc8474
bbbboxvader <tedu0603@naver.com>
2026-09-25 21:37:01 +0900

    화면 띄우기

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,1 +1,25 @@
 # 실습 과제 진행
+from pico2d import*
+
+open_canvas(800,600)
+
+character = load_image('character.png')
+
+def move_circle():
+    print("circle")
+    pass
+
+def move_rectangle():
+    print("rectangle")
+    pass
+
+def move_triangle():
+    print("triangle")
+    pass
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
+    pass
+    
